184cedb37dc0649e591c3f4c92f884df02ece4e8
Jonathan Stucken <jonathan.stucken@swsphn.com.au>
2023-01-23 10:08:12 +1100

    Update with CRLF using VSCODE

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 
 # initial test file created under Ubuntu WSL with LF line endings
-
+# saving file with CRLF in VSCODE. Subsequently the command:
+# file test.py
+#... shows 'ASCII text, with CRLF line terminators'

09262cdfd0dc9b3cf8d0f84d8a762e9ecbc07954
Jonathan Stucken <jonathan.stucken@swsphn.com.au>
2023-01-23 10:26:09 +1100

    Edit in Ubuntu with LF line endings

--- test.py
+++ test.py
@@ -3,3 +3,5 @@
 # saving file with CRLF in VSCODE. Subsequently the command:
 # file test.py
 #... shows 'ASCII text, with CRLF line terminators'
+
+# editing from Ubuntu CLI again with LF line endings...
